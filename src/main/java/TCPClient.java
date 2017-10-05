07385a81c6f173095d82e85d28a8ebbc3a1faf69
Vasile Schidu <sckeedoo@gmail.com>
2017-10-05 18:47:29 +0300

    Added feature to select number of messages and delay time for sending

--- src/main/java/TCPClient.java
+++ src/main/java/TCPClient.java
@@ -4,31 +4,40 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 class TCPClient {
     public static void main(String argv[]) throws Exception {
         String sentence;
         String modifiedSentence;
 
+
+        int maxMess;
+        int delayTime;
         int counter = 0 ;
 
 
             try {
 
-                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
+                Scanner intFromUser = new Scanner(System.in);
                 Socket clientSocket = new Socket("localhost", 8080);
                 DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
                 DataInputStream inFromServer = new DataInputStream(clientSocket.getInputStream());
 
 
-                while (counter < 5) {
+                System.out.println("Enter number of messages you want to send: ");
+                maxMess = intFromUser.nextInt();
+                System.out.println("Enter delay time in seconds for sending: ");
+                delayTime = intFromUser.nextInt();
+
+                while (counter < maxMess) {
                     outToServer.writeUTF("Message " + (counter+1));
                     outToServer.flush();
                     modifiedSentence = inFromServer.readUTF();
                     System.out.println("FROM SERVER: " + "Message" + (counter+1));
                     counter++;
-                    Thread.sleep(5000);
+                    Thread.sleep(delayTime * 1000);
                 }
 
             } catch (IOException e) {
